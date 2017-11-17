6a39b6522054276052469a9b615fe18ccff4b7b8
jcros8 <jjcross@umich.edu>
2017-11-17 18:59:42 -0500

    Removed old code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import BackgroundProvider from './BackgroundProvider/solution';
 
 ReactDOM.render(<App />, document.getElementById('root'));
